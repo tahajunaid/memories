80f7b8c4ecc337c4a1c5ebd5d1b49381540536c7
Taha Junaid <taha.junaid3000@gmail.com>
2023-01-18 03:50:27 -0500

    server setup

--- server/index.js
+++ server/index.js
@@ -2,3 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app=express();
+
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors());
+
+//mongodb+srv://<username>:<password>@cluster0.ybetkmz.mongodb.net/?retryWrites=true&w=majority
+const CONNECTION_URL = 'mongodb+srv://taha_memories:H8o6gkFv9mZK1alg@cluster0.ybetkmz.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=> console.log(error.message));
